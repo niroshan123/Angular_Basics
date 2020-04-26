9b76d02d96c26562e1559a178e68fd4c17ea840d
niroshan123 <niroshanratnayake07@gmail.com>
2020-04-26 23:57:29 +0530

    added rxjs 6 to employee service

--- services-and-http/src/app/services/employee.service.ts
+++ services-and-http/src/app/services/employee.service.ts
@@ -2,29 +2,29 @@ import { Injectable } from '@angular/core';
 import { HttpClient,HttpErrorResponse } from '@angular/common/http';
 //import { Observable } from 'rxjs';
 import { IEmployee } from '../models/employee';
-import { throwError, Observable } from 'rxjs'
-import { catchError } from 'rxjs/operators'
+// import { throwError, Observable } from 'rxjs'
+// import { catchError } from 'rxjs/operators'
+import {throwError as observableThrowError,  Observable } from 'rxjs';
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+// import { IEmployee } from './employee';
+import { tap, catchError } from 'rxjs/operators';
 
 
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class EmployeeService {
 
-  private _url:string="./assets/data/employee.json";
+  private _url: string = "./assets/data/employee.json";
 
   constructor(private http:HttpClient) { }
 
-
-getEmployees():Observable<IEmployee[]>{
-
-return this.http.get<IEmployee[]>(this._url).pipe
-(catchError(this.errorHandler));
-
-}
-errorHandler(error: HttpErrorResponse){
-  return throwError(error.message || "Server Error");
-}
+  getEmployees(): Observable<IEmployee[]>{
+    return this.http.get<IEmployee[]>(this._url)
+                    .pipe(tap(data => alert(JSON.stringify(data))) , catchError(this.errorHandler))
+  }
+  errorHandler(error: HttpErrorResponse){
+    return observableThrowError(error.message || "Server Error");
+  }
 
 }
